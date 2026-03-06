93284106e5d9b49be1d5a9086d1094c3cf229019
pinglan.zw <zhangwang2014@iCloud.com>
2026-03-06 21:06:40 +0800

    Initial Commit

---
+++ 贪吃蛇/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  贪吃蛇
+//
+//  Created by zhangwang on 2026/3/6.
+//
+
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
